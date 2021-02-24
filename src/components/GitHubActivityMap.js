7a94154c885dfedd5e25ba3dcb8686a4c7bfacb3
Chris Spohn <chris.j.spohn@gmail.com>
2021-02-23 18:49:20 -0700

    Add useEffect to fetch github urls and get locations from opencagedata for each event

--- src/components/GitHubActivityMap.js
+++ src/components/GitHubActivityMap.js
@@ -22,6 +22,39 @@ const GitHubActivityMap = (props) => {
     fetchEvents()
   }, [])
 
+  useEffect(() => {
+    //fetch user data
+    const fetchLocations = async () => {
+      const userLocations = await Promise.all(
+        events.map(async (item) => {
+          const result = await fetch(item.actor.url, {
+            headers: {
+              authorization: "token a28d7e80e53dee17c87b109f37a23b3f2f3d3337"
+            }
+          })
+        const data = await result.json();
+        return data
+      }))
+
+    //get locations
+      const places = userLocations.filter(loc => loc.location).map(loc => loc.location);
+      const coords = await Promise.all(
+        places.map(async place => {
+          try {
+            const result = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${place}&key=e0b4a3f2213247859e55897af976da36`)
+            const data = await result.json();
+            return data.results[0].geometry
+          } catch(e) {
+            console.log(`No coordinates for ${place}`)
+          }
+        })
+      )
+      setLocations(coords)
+    }
+    fetchLocations();
+  }, [events])
+
+
   return (
     <div className='github-activity-map-container'>
       { props.error && props.error }
