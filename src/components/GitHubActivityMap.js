3d8e8c1ba95b4734fc9ab22011ed42aa1d71af21
Chris Spohn <chris.j.spohn@gmail.com>
2021-02-23 18:14:54 -0700

    Render leaflet example map from docs

--- src/components/GitHubActivityMap.js
+++ src/components/GitHubActivityMap.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './GitHubActivityMap.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const GitHubActivityMap = (props) => {
 
@@ -9,8 +10,17 @@ const GitHubActivityMap = (props) => {
     <div className='github-activity-map-container'>
       { props.error && props.error }
       {!props.error &&
-        //would be d3 and leaflet stuff
-        <h1>{props.globalGitHubData}</h1>
+        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{height : '100%'}}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
       }
     </div>
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,6 @@ import React from 'react';
 import './Header.css';
 
 const Header = () => {
-
-  console.log('header');
-
   return (
     <header>
       <h1 className='title'>GitHub Visualizer</h1>
