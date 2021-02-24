447d28f4407f0f4391350cdc7ce12a26a4b5fde9
Chris Spohn <chris.j.spohn@gmail.com>
2021-02-23 18:48:07 -0700

    Add state for events locations and markers

--- src/components/GitHubActivityMap.js
+++ src/components/GitHubActivityMap.js
@@ -1,25 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './GitHubActivityMap.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const GitHubActivityMap = (props) => {
 
-  //d3 and leaflet stuff
+  const [events, setEvents] = useState([]);
+  const [locations, setLocations] = useState([]);
+  const [markers, setMarkers] = useState([]);
 
   return (
     <div className='github-activity-map-container'>
       { props.error && props.error }
       {!props.error &&
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{height : '100%'}}>
+        <MapContainer center={[51.505, -0.09]} zoom={1.5} scrollWheelZoom={false} style={{height : '100%'}}>
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
+          {markers.length && markers}
         </MapContainer>
       }
     </div>
