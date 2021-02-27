8a5dc7d0ce9aa98e9ee7478754b51be74cb2354b
Chris Spohn <chris.j.spohn@gmail.com>
2021-02-27 10:32:41 -0700

    Add leaflet night mode

--- src/components/GitHubActivityMap.js
+++ src/components/GitHubActivityMap.js
@@ -117,8 +117,8 @@ const GitHubActivityMap = (props) => {
       {!props.error &&
         <MapContainer center={[51.505, -0.09]} zoom={1.5} scrollWheelZoom={false} style={{height : '100%'}}>
           <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='© <a href="https://stadiamaps.com/">Stadia Maps</a>, © <a href="https://openmaptiles.org/">OpenMapTiles</a> © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+            url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
           />
           {currentMarker && currentMarker}
         </MapContainer>
