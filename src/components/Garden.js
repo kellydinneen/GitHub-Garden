cd71a20698dfab5a1f8a9d023c0bf6e885277692
kellydinneen <kgdinneen@gmail.com>
2021-02-26 21:40:53 -0700

    Finetune root rotation

--- src/components/Garden.js
+++ src/components/Garden.js
@@ -21,6 +21,28 @@ const Garden = (props) => {
       .domain([minLifespan, maxLifespan])
       .range([gardenHeight/2, gardenHeight/3, gardenHeight/4, gardenHeight/5])
 
+      const stem = flowerBed.selectAll('.stem')
+        .data(repositories).enter()
+        .append('path')
+        .attr('class','stem')
+        .attr('id', (d, i) => `myStem${i}`)
+        .attr('d', d => `M0,600 C 80 ${yStemScale(d.lifespan)}, -20 ${yStemScale(d.lifespan)}, 0 ${yStemScale(d.lifespan)}`)
+        .attr('transform', (d, i) => `translate(${100 + i * 200},0)`)
+        .attr('stroke-width', 5)
+        .attr('stroke', 'green')
+        .attr('fill', 'none')
+
+        const animate = () => {
+          flowerBed.selectAll('.stem')
+            .transition().duration(2000)
+            .attr('d', d => `M0,600 C -20 ${yStemScale(d.lifespan)}, 20 ${yStemScale(d.lifespan)}, 0 ${yStemScale(d.lifespan)}`)
+              .attr('stroke', 'green')
+            .transition().duration(2000)
+            .attr('d', d => `M0,600 C 80 ${yStemScale(d.lifespan)}, -20 ${yStemScale(d.lifespan)}, 0 ${yStemScale(d.lifespan)}`)
+            .on("end", animate)
+        }
+      animate();
+
     const flowerSizeScale = d3.scaleQuantize()
       .domain([minLifespan, maxLifespan])
       .range([0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1])
@@ -94,29 +116,6 @@ const Garden = (props) => {
       .attr('stroke', 'blue')
       .attr('fill', 'blue')
 
-    const stem = flowerBed.selectAll('.stem')
-      .data(repositories).enter()
-      .append('path')
-      .attr('class','stem')
-      .attr('id', (d, i) => `myStem${i}`)
-      .attr('d', d => `M0,600 C 80 ${yStemScale(d.lifespan)}, -20 ${yStemScale(d.lifespan)}, 0 ${yStemScale(d.lifespan)}`)
-      .attr('transform', (d, i) => `translate(${100 + i * 200},0)`)
-      .attr('stroke-width', 5)
-      .attr('stroke', 'green')
-      .attr('fill', 'none')
-
-      const animate = () => {
-        flowerBed.selectAll('.stem')
-          .transition().duration(2000)
-          .attr('d', d => `M0,600 C -20 ${yStemScale(d.lifespan)}, 20 ${yStemScale(d.lifespan)}, 0 ${yStemScale(d.lifespan)}`)
-            .attr('stroke', 'green')
-          .transition().duration(2000)
-          .attr('d', d => `M0,600 C 80 ${yStemScale(d.lifespan)}, -20 ${yStemScale(d.lifespan)}, 0 ${yStemScale(d.lifespan)}`)
-          .on("end", animate)
-      }
-    animate();
-
-
       flowerBed.selectAll('.repoName')
         .data(repositories).enter()
         .append('text')
@@ -139,7 +138,7 @@ const Garden = (props) => {
           return {
             repo: d.name,
             name: branch,
-            rotationFactor: d.branches.length === 1 ? 0 : 160 / d.branches.length
+            rotationFactor: d.branches.length === 1 ? -280 : 160 / d.branches.length
           }
         }))
         .enter().append('path')
@@ -153,7 +152,7 @@ const Garden = (props) => {
           const index = (array) => [Math.floor(Math.random() * array.length)];
           return `M0,0 C-10,40 ${arrayOfCurves[index(arrayOfCurves)]},100 0,${arrayOfLengths[index(arrayOfLengths)]}`;
         })
-        .attr('transform', (d,i) => `rotate(${280 + i * d.rotationFactor})`)
+        .attr('transform', (d,i) => `rotate(${260 + (i + 1) * d.rotationFactor})`)
 
     }
 
