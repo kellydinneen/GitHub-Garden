4d092de314a35f9c00e6f3980b77de1d9f9e4ec9
kellydinneen <kgdinneen@gmail.com>
2021-02-28 19:24:37 -0700

    Add soil lines according to numbers of repositories

--- src/components/Garden.js
+++ src/components/Garden.js
@@ -11,6 +11,24 @@ const Garden = (props) => {
   const drawGarden = () => {
     const flowerBed = d3.select('.flowerbed');
 
+    const soilLine = flowerBed.selectAll('.soil-line')
+      .data(() => {
+        let soilLines = [];
+        repositories.forEach((repo, i) => {
+          if(i % 5 === 0) {
+            soilLines.push('soil');
+          }
+        })
+        return soilLines;
+      })
+      .enter().append('path')
+      .attr('class', '.soil-line')
+      .attr('d', 'M0,400 C100,300 150,450 200,410 C250,370 270,420 370,410 C450,370 470,420 480,360 C500,370 520,420 560,400 C600,370 620,420 660,400 C700,370 720,420 760,400 C800,300 850,450 900,410 C950,370 970,420 980,360 C1050,370 1070,420 1080,360 C1200,370 1220,420 1260,400 C1300,300 1350,450 1400,410 C1450,370 1470,420 1570,410 C1650,370 1670,420 1680,360 C1750,370 1770,420 1780,360 C1800,370 1820,420 1860,400 C1900,300 1950,450 2000,410')
+      .attr('transform', (d, i) => `translate(${i*1000}, 395)scale(0.5)`)
+      .attr('stroke', 'grey')
+      .attr('stroke-width', '3px')
+      .attr('fill', 'none')
+
     const maxLifespan = d3.max(repositories, d => d.lifespan)
     const minLifespan = d3.min(repositories, d => d.lifespan)
 
@@ -156,7 +174,7 @@ const Garden = (props) => {
   }, [])
 
   return (
-    <svg className='flowerbed' viewBox='0 0 2000 800' width={gardenWidth}></svg>
+      <svg className='flowerbed' viewBox='0 0 2000 800' width={gardenWidth}></svg>
   )
 }
 
