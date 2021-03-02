9b6519861eb42236eb6196947245addcd7c2ea8a
Chris Spohn <chris.j.spohn@gmail.com>
2021-03-02 15:00:13 -0700

    Render key only if there is no GitHubError

--- src/components/ProfileVisualization.js
+++ src/components/ProfileVisualization.js
@@ -133,7 +133,7 @@ const ProfileVisualization = (props) => {
         setCleanUserData(consolidatedData);
         setTimeout(() => {setIsLoaded(true)}, 4000);
       } else {
-        await setError(true)
+        await setGitHubError(true)
         setIsLoaded(true)
       }
     }
